8bb86567ba85e7d7526c85a1ffe8cbae5467726e
Joseph <JosephSchander@gmail.com>
2021-09-11 18:18:20 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class App
+{
+
+    public static void main( String[] args )
+    {
+        DecimalFormat DF = new DecimalFormat("##.##");
+        Scanner uInput = new Scanner(System.in);
+        System.out.println( "Enter the principal" );
+        double Principal = uInput.nextDouble();
+        System.out.println("Enter the percent rate of interest");
+        double ROA = uInput.nextDouble();
+        System.out.println("Enter the number of years");
+        int Years = uInput.nextInt();
+        System.out.println("What is the number of times the interest is compounded per year?");
+        int Comp = uInput.nextInt();
+        double m = (1+((ROA/100)/Comp));
+        double n = Years * Comp;
+        System.out.println("After " + Years + " years at "+ ROA +"%, compounded "+ Comp+ " times per year, the investment will be worth $"+ DF.format(Principal*(java.lang.Math.pow(m,n))));
+    }
+}
